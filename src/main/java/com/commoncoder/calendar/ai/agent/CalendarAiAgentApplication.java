d4785acef286c03ca47c6a52d0b0d76c739fc47f
Vatsal Mehta <vatsaldmehta@Vatsals-MacBook-Pro.local>
2025-12-21 19:28:21 +0530

    Add boilerplate code for spring boot and its integration with gemini

---
+++ src/main/java/com/commoncoder/calendar/ai/agent/CalendarAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.commoncoder.calendar.ai.agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarAiAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalendarAiAgentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/commoncoder/calendar/ai/agent/CalendarAiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commoncoder.calendar.ai.agent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarAiAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
